297ac8bb1db822e3f31da157a927fae3d88a5a82
flyisland <flyisland@gmail.com>
2021-03-18 12:29:21 +0800

    add more test cases

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,26 +7,73 @@ import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
 import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class App {
-    public static final String JSON_BOOK_DOCUMENT =
-            "{ " +
-                    "   \"category\" : \"reference\",\n" +
-                    "   \"author\" : \"Nigel Rees\",\n" +
-                    "   \"title\" : \"Sayings of the Century\",\n" +
-                    "   \"display-price\" : 8.95\n" +
+    private static final String JSON_BOOK_DOCUMENT =
+            "{\n" +
+                    "    \"store\": {\n" +
+                    "        \"book\": [\n" +
+                    "            {\n" +
+                    "                \"category\": \"reference\",\n" +
+                    "                \"author\": \"Nigel Rees\",\n" +
+                    "                \"title\": \"Sayings of the Century\",\n" +
+                    "                \"price\": 8.95\n" +
+                    "            },\n" +
+                    "            {\n" +
+                    "                \"category\": \"fiction\",\n" +
+                    "                \"author\": \"Evelyn Waugh\",\n" +
+                    "                \"title\": \"Sword of Honour\",\n" +
+                    "                \"price\": 12.99\n" +
+                    "            },\n" +
+                    "            {\n" +
+                    "                \"category\": \"fiction\",\n" +
+                    "                \"author\": \"Herman Melville\",\n" +
+                    "                \"title\": \"Moby Dick\",\n" +
+                    "                \"isbn\": \"0-553-21311-3\",\n" +
+                    "                \"price\": 8.99\n" +
+                    "            },\n" +
+                    "            {\n" +
+                    "                \"category\": \"fiction\",\n" +
+                    "                \"author\": \"J. R. R. Tolkien\",\n" +
+                    "                \"title\": \"The Lord of the Rings\",\n" +
+                    "                \"isbn\": \"0-395-19395-8\",\n" +
+                    "                \"price\": 22.99\n" +
+                    "            }\n" +
+                    "        ],\n" +
+                    "        \"bicycle\": {\n" +
+                    "            \"color\": \"red\",\n" +
+                    "            \"price\": 19.95\n" +
+                    "        }\n" +
+                    "    },\n" +
+                    "    \"expensive\": 10\n" +
                     "}";
+    private static Configuration jsonPathConf;
 
     public static void main(String[] args) {
-        Configuration jsonPathConf = Configuration
+        var b = Configuration
                 .builder()
                 .mappingProvider(new JacksonMappingProvider())
-                .jsonProvider(new JacksonJsonProvider())
-                .options(Option.SUPPRESS_EXCEPTIONS)
-                .build();
+                .jsonProvider(new JacksonJsonProvider());
 
-        var keys = JsonPath.using(jsonPathConf).parse(JSON_BOOK_DOCUMENT)
-                .read("$.keys()", List.class);
-        System.out.println("The keys are " + keys);
+        if (args.length == 1 && args[0].equalsIgnoreCase("SUPPRESS_EXCEPTIONS")){
+            b.options(Option.SUPPRESS_EXCEPTIONS);
+        }
+        jsonPathConf = b.build();
+
+        test("$.store.bicycle.color", String.class);
+        test("$.store.book[*].author", List.class);
+        test("$.store.book[?(@.price > 20)]", List.class);
+        test("$.keys()", List.class);
+    }
+
+    private static <T> void test(String path, Class<T> type) {
+        var value = JsonPath.using(jsonPathConf).parse(JSON_BOOK_DOCUMENT)
+                .read(path, type);
+        System.out.printf("Read '%s' returns [%s]: %s%n", path,
+                Optional.ofNullable(value).map(v ->v.getClass().getCanonicalName()).orElse("null"),
+                value
+        );
     }
 }
